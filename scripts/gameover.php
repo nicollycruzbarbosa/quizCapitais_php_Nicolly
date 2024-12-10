86abfc079135d39e3a383b23fe07559ccadbb632
nicollycruzbarbosa <nicollybarbosaeduc@gmail.com>
2024-12-10 11:21:54 -0300

    Initial commit

---
+++ scripts/gameover.php
@@ -0,0 +1,14 @@
+<?php
+
+    // Digitar o php aqui
+
+?>
+
+<!-- Início do código HTML para exibir os resultados finais do jogo -->
+<div class="result-container">   
+   
+    <!-- Digitar o html aqui -->
+
+</div>
+
+
---
+++ scripts/start.php
@@ -0,0 +1,12 @@
+<?php
+
+    // Digitar o php aqui
+
+?>
+
+<!-- Início do código HTML para exibir o formulário na página de início -->
+<div class="container">
+
+    <!-- Digitar o html aqui -->
+
+</div>
